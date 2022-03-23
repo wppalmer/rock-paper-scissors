e245f10bc7a6fce62ad5eab289a61016b1c24778
William Palmer <wppalmer95@icloud.com>
2022-03-22 22:02:05 -0500

    added strict mode

---
+++ game.js
@@ -0,0 +1,6 @@
+'use strict'
+
+const compArray = [rock, paper, scissors];
+const rock = document.getElementById(rock);
+const paper = document.getElementById(paper);
+const scissors = document.getElementById(scissors);
