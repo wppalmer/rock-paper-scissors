ab475897eb0a70d96de92fb631cd5d1fab769118
William Palmer <wppalmer95@icloud.com>
2022-04-01 20:56:29 -0500

    added button functions

--- game.js
+++ game.js
@@ -1,6 +1,41 @@
 'use strict'
 
-const compArray = [rock, paper, scissors];
-const rock = document.getElementById(rock);
-const paper = document.getElementById(paper);
-const scissors = document.getElementById(scissors);
+const compArray = ['rock', 'paper', 'scissors'];
+const rock = document.getElementById('rock1');
+const paper = document.getElementById('paper1');
+const scissors = document.getElementById('scissors1');
+
+//Button Inputs
+let playerChoice, compChoice;
+
+const playerRock = function(){
+    rock.addEventListener('click', function(){
+        playerChoice = 'rock'
+        console.log(playerChoice)
+    })
+}
+const playerPaper = function(){
+    paper.addEventListener('click', function(){
+        playerChoice = 'paper'
+        console.log(playerChoice)
+    })
+}
+const playerScissors = function(){
+    scissors.addEventListener('click', function(){
+        playerChoice = 'scissors'
+        console.log(playerChoice)
+    })
+}
+playerRock();
+playerPaper();
+playerScissors();
+
+//Game Comparison
+
+//Display Message
+
+//Tally Score
+
+//Reset Round
+
+//End Game
