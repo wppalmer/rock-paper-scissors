46826ce46fbdbceb6d09e24658b908e40ded46ed
William Palmer <wppalmer95@icloud.com>
2022-05-16 05:47:46 -0500

    added computer choice function.

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 const compArray = ['rock', 'paper', 'scissors'];
 const rock = document.getElementById('rock1');
@@ -6,33 +6,36 @@ const paper = document.getElementById('paper1');
 const scissors = document.getElementById('scissors1');
 
 //Button Inputs
-let playerChoice, compChoice;
+let playerChoice, compChoice, scores, currentScore;
 
-const playerRock = function(){
-    rock.addEventListener('click', function(){
-        playerChoice = 'rock'
-        console.log(playerChoice)
-    })
-}
-const playerPaper = function(){
-    paper.addEventListener('click', function(){
-        playerChoice = 'paper'
-        console.log(playerChoice)
-    })
-}
-const playerScissors = function(){
-    scissors.addEventListener('click', function(){
-        playerChoice = 'scissors'
-        console.log(playerChoice)
-    })
-}
+const playerRock = function () {
+  rock.addEventListener('click', function () {
+    playerChoice = 'rock';
+    console.log(playerChoice);
+  });
+};
+const playerPaper = function () {
+  paper.addEventListener('click', function () {
+    playerChoice = 'paper';
+    console.log(playerChoice);
+  });
+};
+const playerScissors = function () {
+  scissors.addEventListener('click', function () {
+    playerChoice = 'scissors';
+    console.log(playerChoice);
+  });
+};
 playerRock();
 playerPaper();
 playerScissors();
 
-compChoice = function() {
-   
-}
+//Computer Selection
+compChoice = function () {
+  let random = Math.floor(Math.random() * compArray.length);
+  compChoice = compArray[random];
+  console.log(compChoice);
+};
 
 //Game Comparison
 
