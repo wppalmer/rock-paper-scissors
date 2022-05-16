7fead3dd0dc1e5fc87deca35940194ce8dda6923
William Palmer <wppalmer95@icloud.com>
2022-05-16 05:53:22 -0500

    cleaned up player selection function.

--- game.js
+++ game.js
@@ -5,33 +5,27 @@ const rock = document.getElementById('rock1');
 const paper = document.getElementById('paper1');
 const scissors = document.getElementById('scissors1');
 
-//Button Inputs
 let playerChoice, compChoice, scores, currentScore;
 
-const playerRock = function () {
+//Player Selection
+const playerSelection = function () {
   rock.addEventListener('click', function () {
     playerChoice = 'rock';
     console.log(playerChoice);
   });
-};
-const playerPaper = function () {
   paper.addEventListener('click', function () {
     playerChoice = 'paper';
     console.log(playerChoice);
   });
-};
-const playerScissors = function () {
   scissors.addEventListener('click', function () {
     playerChoice = 'scissors';
     console.log(playerChoice);
   });
 };
-playerRock();
-playerPaper();
-playerScissors();
+playerSelection();
 
 //Computer Selection
-compChoice = function () {
+const compSelection = function () {
   let random = Math.floor(Math.random() * compArray.length);
   compChoice = compArray[random];
   console.log(compChoice);
