92e8b70bf8849b055ed23558ae186f8d29fe2f48
William Palmer <wppalmer95@icloud.com>
2022-05-20 19:57:34 -0600

    started reset function

--- menu.js
+++ menu.js
@@ -1,1 +1,12 @@
 'use strict'
+
+const reset = document.getElementById('btn-reset');
+let action
+
+const gameReset = function() {
+    reset.addEventListener('click', function(){
+        console.log('Reset button was hit')
+    })
+}
+
+gameReset()
