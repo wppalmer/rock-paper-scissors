4f5c373128e9804fb8fabd5f9060e5a869e9eff1
William Palmer <wppalmer95@icloud.com>
2022-05-20 20:28:43 -0600

    fixed selection function

--- game.js
+++ game.js
@@ -4,35 +4,44 @@ const compArray = ['rock', 'paper', 'scissors'];
 const rock = document.getElementById('rock1');
 const paper = document.getElementById('paper1');
 const scissors = document.getElementById('scissors1');
-
+const playerWins = 'Player wins'
+const compWins = 'Computer wins'
 
 let playerChoice, compChoice, scores, currentScore;
+
+//Computer Selection
+const compSelection = function () {
+    let random = Math.floor(Math.random() * compArray.length);
+    compChoice = compArray[random];
+    console.log(compChoice);
+  };
+  
 
 //Player Selection
 const playerSelection = function () {
   rock.addEventListener('click', function () {
     playerChoice = 'rock';
     console.log(playerChoice);
+    compSelection()
   });
   paper.addEventListener('click', function () {
     playerChoice = 'paper';
     console.log(playerChoice);
+    compSelection()
   });
   scissors.addEventListener('click', function () {
     playerChoice = 'scissors';
     console.log(playerChoice);
+    compSelection()
   });
 };
 playerSelection();
 
-//Computer Selection
-const compSelection = function () {
-  let random = Math.floor(Math.random() * compArray.length);
-  compChoice = compArray[random];
-  console.log(compChoice);
-};
 
 //Game Comparison
+if(playerChoice = 'rock' && compChoice = 'scissors') {
+    console.log(playerWins)
+}
 
 //Display Message
 
